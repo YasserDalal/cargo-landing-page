ab8440b37ae8cadf7f4007ac7a426a12e00e3662
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-09-23 21:23:47 +0300

    add position fixed to Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 export default function App() {
   return (
     <div className='min-w-80'>
-      <Header className={'flex justify-between items-center bg-black text-white py-2'} />
+      <Header className={'flex justify-between items-center bg-black text-white py-2 fixed w-full z-50'} />
     </div>
   )
 }
