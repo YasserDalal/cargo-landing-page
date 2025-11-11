a9dc4de3916d29698846cfe764da16639f0bbd99
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-11-11 15:37:34 +0300

    feat: update title/favicon

--- src/components/Center.jsx
+++ src/components/Center.jsx
@@ -1,3 +1,14 @@
+import { useEffect } from 'react';
+import { useLocation } from "react-router-dom";
+
 export default function Center({ className, children }) {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    const title = document.querySelector('title')
+    const routePath = pathname.slice(1).charAt(0).toUpperCase() + pathname.slice(2);
+    const centerDot = routePath ? ' • ' : '';
+
+    title.textContent = 'Five Star' + centerDot + routePath
+  }, [pathname])
   return <div className={className}>{children}</div>;
 }
